55c3eaac04e5fadefd2c58c8ae747bbdb326fc2b
andiigela <andiigela@gmail.com>
2024-02-26 11:00:14 +0100

    Created database configurations

---
+++ myapp/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/firstfastapi";
+
+engine = create_engine()
+
+session = sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+Base = declarative_base();
+
+
