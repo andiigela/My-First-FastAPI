462a428127a640c08b38d104ef7ba907c16610ed
andiigela <andiigela@gmail.com>
2024-02-26 11:10:12 +0100

    Mapped models to database entities using sqlalchemy orm

---
+++ myapp/dbentities.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from myapp.database import Base
+class Faculty(Base):
+    __tablename__ = 'faculty'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, index=True)
+
+class Student(Base):
+    __tablename__ = 'student'
+    id = Column(Integer, primary_key=True)
+    full_name = Column(String, index=True)
+    age = Column(Integer)
+    faculty_id = Column(Integer, ForeignKey("faculty.id"))
