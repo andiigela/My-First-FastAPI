70a6cf9be8b071f90e1d9ad8b08fe209cd5d040a
andiigela <andiigela@gmail.com>
2024-02-26 11:20:49 +0100

    Created session with database and dependency for decoupling

--- myapp/main.py
+++ myapp/main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI;
+from fastapi import FastAPI, Depends;
+from myapp import database
+from myapp.database import engine, session
+
 app = FastAPI();
+
+database.Base.metadata.create_all(bind=engine)
+def get_db():
+    sessionDb = session();
+    try:
+        yield sessionDb
+    finally:
+        sessionDb.close();
+
+db_dependency = Depends(get_db)
+
 @app.get("/")
 def hello_world():
     return "Hello World";
 
-
+@app.post("/api/v1/faculty/create")
+def create_faculty():
+    pass;
